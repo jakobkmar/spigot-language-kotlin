a678b59f8470fa507fd15f697f61bc53776ffe5f
Jakob K <bluefireolymp@gmail.com>
2021-05-18 18:42:19 +0200

    Add spigot dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,11 @@ version = "0.0.1"
 
 repositories {
     mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
 }
