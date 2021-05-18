a46dee2ffc2337059bd56f756e382c317c6799f3
Jakob K <bluefireolymp@gmail.com>
2021-05-18 19:58:58 +0200

    Publish to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     kotlin("jvm") version "1.5.0"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    `maven-publish`
 }
+
+val githubRepo = "bluefireoly/spigot-language-kotlin"
 
 group = "net.axay"
 version = "0.0.1"
+
+description = "A Spigot plugin including the Kotlin stdlib and all common kotlinx libraries"
 
 repositories {
     mavenCentral()
@@ -26,9 +31,11 @@ dependencies {
 tasks {
     processResources {
         inputs.property("version", project.version)
+        inputs.property("description", project.description)
 
         filesMatching("plugin.yml") {
             expand("version" to project.version)
+            expand("description" to project.description)
         }
     }
 
@@ -39,5 +46,48 @@ tasks {
 
     compileKotlin {
         kotlinOptions.jvmTarget = "11"
+    }
+
+publishing {
+    repositories {
+        maven("https://oss.sonatype.org/service/local/staging/deploy/maven2") {
+            name = "ossrh"
+            credentials(PasswordCredentials::class)
+        }
+    }
+
+    publications {
+        create<MavenPublication>(project.name) {
+            from(components["java"])
+
+            this.groupId = project.group.toString()
+            this.artifactId = project.name.toLowerCase()
+            this.version = project.version.toString()
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+
+                developers {
+                    developer {
+                        name.set("bluefireoly")
+                    }
+                }
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                url.set("https://github.com/${githubRepo}")
+
+                scm {
+                    connection.set("scm:git:git://github.com/${githubRepo}.git")
+                    url.set("https://github.com/${githubRepo}/tree/main")
+                }
+            }
+        }
     }
 }
