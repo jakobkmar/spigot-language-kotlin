70ae89eda52b4fb66df426fe24b6594a0d21d098
Jakob K <bluefireolymp@gmail.com>
2021-05-18 18:50:56 +0200

    Set the version in the plugin.yml to the project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,13 @@ dependencies {
 
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
 }
+
+tasks {
+    processResources {
+        inputs.property("version", project.version)
+
+        filesMatching("plugin.yml") {
+            expand("version" to project.version)
+        }
+    }
+}
